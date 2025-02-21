7c282dd05dc7faf96a98733e225d3ff29649ed0f
ThiX <thiagomwd@gmail.com>
2025-02-21 10:47:56 -0300

    checkpoint 2 - dealing with handler func

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Message struct {
+	Text string `json:"text"`
+	Age  int    `json:"age_in_seconds"`
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	message := Message{Text: "Bem-vindo à nossa API!"}
+	message.Age = 30 * 24 * 60 * 60
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
 
 func main() {
-	fmt.Println("Olá, mundo!")
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Servidor iniciado na porta 8080")
+	http.ListenAndServe(":8080", nil)
 }
